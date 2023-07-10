fa61ace21e650e765f895fba2aca66ede9314a86
Ansaf Nisam <80558066+camkon@users.noreply.github.com>
2023-07-10 16:26:05 +0530

    action types

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,10 +1,11 @@
 import * as api from '../api'
+import { CREATE, DELETE, LIKE_POST, READ, UPDATE } from '../constants/actionTypes'
 
 //ACTION CREATORS
 export const getPosts = () => async (dispatch) => {
     try {
         const {data} = await api.fetchPosts()
-        dispatch({type: 'READ', payload: data})
+        dispatch({type: READ, payload: data})
     } catch (error) {
         console.log(error?.message)
     }
@@ -13,7 +14,7 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = (post) => async (dispatch) => {
     try {
         const {data} = await api.createPost(post)
-        dispatch({type: 'CREATE', payload: data})
+        dispatch({type: CREATE, payload: data})
     } catch (error) {
         console.log(error?.message)
     }
@@ -22,7 +23,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
     try {
         const {data} = await api.updatePost(id, post)
-        dispatch({type: 'UPDATE', payload: data})
+        dispatch({type: UPDATE, payload: data})
         dispatch(getPosts())
     } catch (error) {
         console.log(error?.response?.data?.message)
@@ -32,7 +33,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
     try {
         await api.deletePost(id)
-        dispatch({type: 'DELETE', payload: id})
+        dispatch({type: DELETE, payload: id})
     } catch (error) {
         console.log(error)
     }
@@ -41,7 +42,7 @@ export const deletePost = (id) => async (dispatch) => {
 export const likePost = (id) => async (dispatch) => {
     try {
         const {data} = await api.likePost(id)
-        dispatch({type: 'LIKE_POST', payload: data})
+        dispatch({type: LIKE_POST, payload: data})
     } catch (error) {
         console.log(error)
     }
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,13 +1,15 @@
+import { CREATE, DELETE, LIKE_POST, READ, UPDATE } from '../constants/actionTypes.js'
+
 export default (posts = [], action) => {
     switch (action.type) {
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload]
-        case 'READ':
+        case READ:
             return action.payload
-        case 'UPDATE':
-        case 'LIKE_POST':
+        case UPDATE:
+        case LIKE_POST:
             return posts.map((item) => item._id === action.payload._id ? action.payload : item)
-        case 'DELETE':
+        case DELETE:
             return posts.filter((item) => item._id !== action.payload)
         default:
             return posts
