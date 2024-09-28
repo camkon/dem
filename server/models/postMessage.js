9a0dfb10af6435dcf3585b5c35965f9a28598e91
Ansaf Nisam <80558066+camkon@users.noreply.github.com>
2024-09-28 23:57:51 +0530

    Update postMessage.js

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,7 +1,7 @@
 /* This code is defining a Mongoose schema for a post, which includes fields such as title, message,
 creator, tags, selectedFile, likeCount, and createdAt. It then creates a Mongoose model for this
 schema called "PostMessage" and exports it as the default export of the module. This allows other
-parts of the application to import and use this model to interact with a MongoDB database. */
+parts of the application to import and use this model to interact with a MongoDB db. */
 import mongoose from 'mongoose'
 
 const postSchema = mongoose.Schema({
